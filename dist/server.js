b0d43eafcb29798c4ab2a270bf7de04cf85145a6
Nasser Farhat <nasserfarhatt@gmail.com>
2020-08-05 14:06:19 -0300

    ESLint & Jest

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,12 @@
-"use strict";
+'use strict'
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = express_1.default();
+  return (mod && mod.__esModule) ? mod : { default: mod }
+}
+Object.defineProperty(exports, '__esModule', { value: true })
+// eslint-disable-next-line camelcase
+const express_1 = __importDefault(require('express'))
+const app = express_1.default()
 app.get('/', (request, response) => {
-    return response.json({ message: "Hello World!" });
-});
-app.listen(3333);
+  return response.json({ message: 'Hello World!' })
+})
+app.listen(3333)
